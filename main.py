2f02f854702173c39c40f58aea83edec3d6ebae1
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-02-22 23:44:04 +0100

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('Hello world')
+number = int(input("Введіть будь ласка 4-х значне число: "))
+print(number // 1000)
+print((number // 100) % 10)
+print((number // 10) % 10)
+print(number % 10)
