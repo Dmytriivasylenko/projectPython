dd6a3237ed0fe6ee585e638029067402b88fbeb1
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-02-23 10:32:40 +0100

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-number = int(input("Введіть 5-ти значне число: "))
+number = int(input("Введіть будь ласка 4-х значне число: "))
 
-reversed_number = (number % 10) * 10000 + ((number // 10) % 10) * 1000 + ((number // 100) % 10) * 100 + ((number // 1000) % 10) * 10 + ((number // 10000) % 100)
-
-print("Зворотнє число:", reversed_number)
+print(number // 1000)
+print((number // 100) % 10)
+print((number // 10) % 10)
+print(number % 10)
