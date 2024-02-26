936c97ff94ab99b5b5f6602dfa78f8d6869a6134
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-02-26 22:16:28 +0100

    1

--- main2.py
+++ main2.py
@@ -1,28 +1,23 @@
-def calculator():
-    while True:
-        try:
-            num1 = float(input("Please enter first number: "))
-            num2 = float(input("Please enter second number: "))
-            action = input("Please enter action (+, -, *, /): ")
-        if action == "+":
-            result = num1 + num2
-        elif action == "-":
-            result = num1 - num2
-        elif action == "*":
-            result = num1 * num2
-        elif action == "/":
-            result = num1 / num2
-            if num2 == 0:
-                print("You can't dived by 0")
-                continue
-            else:
-                result = num1 / num2
-        else:
-            print("Invalid action")
-            continue
+while True:
+    number1 = float(input("Please enter first number: "))
+    number2 = float(input("Please enter second number: "))
+    action = input("Please enter action (+, -, *, /): ")
 
-        print("Your result is" result)
-        break
-except ValueError:
-        print("Please enter valid numbers")
-calculator()
+    if action == '+':
+        result = number1 + number2
+    elif action == '-':
+        result = number1 - number2
+    elif action == '*':
+        result = number1 * number2
+    elif action == '/':
+        if number2 != 0:
+            result = number1 / number2
+        else:
+            print("You can't divide by 0")
+            continue
+    else:
+        print("Invalid action")
+        continue
+
+    print("Your result is", result)
+    break
