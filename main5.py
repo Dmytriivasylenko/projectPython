852c37bd05b8dff43c21d223680d31bb69bdc303
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-03-07 21:55:29 +0100

    1

--- main5.py
+++ main5.py
@@ -4,6 +4,7 @@
 #[1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
 #[9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 
-def move_zero(lst):
-    zeros = [x for x in lst if x == 0]
-    lst.extend(zeros)
+def move_zeros(lst):
+    non_zeros = [x for x in lst if x != 0]
+    zeros = [0] * (len(lst) - len(non_zeros))
+    return non_zeros + zeros
