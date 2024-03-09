bb34b1286b263eba618bb85268c65503d9d3f2a4
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-03-09 09:50:11 +0100

    1

---
+++ main9.py
@@ -0,0 +1,26 @@
+while True:
+    number1 = float(input("Please enter first number: "))
+    number2 = float(input("Please enter second number: "))
+    action = input("Please enter action (+, -, *, /): ")
+
+    if action == '+':
+        result = number1 + number2
+    elif action == '-':
+        result = number1 - number2
+    elif action == '*':
+        result = number1 * number2
+    elif action == '/':
+        if number2 != 0:
+            result = number1 / number2
+        else:
+            print("You can't divide by 0")
+            continue
+    else:
+        print("Invalid action")
+        continue
+
+    print("Your result is", result)
+
+    choice = input("Do you want to perform another calculation? (yes): ")
+    if choice.lower() != 'yes' and choice.lower() != 'y':
+        break
