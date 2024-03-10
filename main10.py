d5b23de6b96522c38cc9cc9dad185e5e8d043f9c
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-03-10 18:39:37 +0100

    1

--- main10.py
+++ main10.py
@@ -1,12 +1,13 @@
-def hashtag():
-    s = input("Enter a phrase: ")
-    cleaned_s = ''.join(char for char in s if char.isalnum())
-
-    hashtag = '#' + ''.join(word.capitalize() for word in cleaned_s.split())
+def project1():
+    s = input("Enter some word: ")
+    cleaned_s = ''.join(char for char in s if char.isalnum() or char.isspace())
+    words = cleaned_s.split()
+    hashtag = '#' + ''.join(word.capitalize() for word in words)
 
     if len(hashtag) > 140:
         hashtag = hashtag[:140]
 
     return hashtag
 
-print(hashtag())
+# Приклад
+print(project1())
