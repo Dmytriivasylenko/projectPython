eb957af3fbd3486471ee462f9e256c7636e4ec95
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-03-18 21:36:41 +0100

    1

--- main13.py
+++ main13.py
@@ -2,12 +2,28 @@ def time_1(seconds):
     days, seconds = divmod(seconds, 86400)
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
-    return f"{days} {'day' if days == 1 else 'days'}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 
-user_input = int(input("Enter the number of seconds: "))
+    if days == 1:
+        days_str = "день"
+    elif 2 <= days <= 4:
+        days_str = "дні"
+    else:
+        days_str = "днів"
 
-if 0 <= user_input < 8640000:
-    result = time_1(user_input)
-    print("Time in a readable format:", result)
-else:
-    print("The entered number does not match the task conditions.")
+    time_str = f"{int(days)} {days_str}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+    return time_str
+
+def input_seconds():
+    while True:
+        try:
+            seconds = int(input("введіть число більше або дорівнює 0 і менше ніж 8640000.):"))
+            if seconds >= 0 and seconds <= 8640000:
+                return seconds
+            else:
+                print("Неправильне значення!Введіть число більше або дорівнює 0 і менше ніж 8640000.")
+        except ValueError:
+            print("Будь ласка, введіть ціле число.")
+
+seconds =input_seconds()
+
+print(f"Формат часу: {time_1(seconds)}") #результат
